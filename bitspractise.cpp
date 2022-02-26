d74c161f418dbf399c5ef1f37f8b15ead618d465
AmodSeth <86409774+AmodSeth@users.noreply.github.com>
2022-02-26 18:32:17 +0530

    Update bitspractise.cpp

--- bitspractise.cpp
+++ bitspractise.cpp
@@ -6,13 +6,15 @@ int main()
 	freopen("input.txt","r", stdin);
 	freopen("output.txt","w", stdout);
 #endif
+	
+	//to check whether the number is in power of 2 or not o(1) soln
 	int n; cin>>n;
 
 	cout<<(~(n-1))<<endl;
 	cout<<n<<endl;
 
 
-	cout<<(n&(~(n-1)))<<endl;
+	cout<<(n&(~(n-1)))==n<<endl;
 
 
 
